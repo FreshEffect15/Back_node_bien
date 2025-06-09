13c87ff90a00f5924f604edfea5bf618d74cdc0e
FreshEffect15 <gossuinulises@gmail.com>
2025-06-09 16:39:10 -0300

    Se muestra el nombre de las categorias también

--- src/services/productoService.ts
+++ src/services/productoService.ts
@@ -3,11 +3,31 @@ import { PrismaClient } from '../generated/prisma';
 const prisma = new PrismaClient();
 
 export const getAllProductos = async () => {
-  return prisma.producto.findMany({ where: { activo: true } });
+  return prisma.producto.findMany({
+    where: { activo: true },
+    include: {
+      categoria: {
+        select: {
+          id: true,
+          nombre: true,
+        },
+      },
+    },
+  });
 };
 
 export const getProductoById = async (id: number) => {
-  return prisma.producto.findFirst({ where: { id, activo: true } });
+  return prisma.producto.findFirst({
+    where: { id, activo: true },
+    include: {
+      categoria: {
+        select: {
+          id: true,
+          nombre: true,
+        },
+      },
+    },
+  });
 };
 
 export const createProducto = async (data: any) => {
